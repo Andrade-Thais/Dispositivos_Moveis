47b00e663cf3a02e39b4f08808ec2118bfca5e66
Andrade-Thais <thais.andrade11@fatec.sp.gov.br>
2024-08-28 10:03:39 -0300

    aula_04

--- Aula_04/app/src/main/java/com/example/aula_04/Agenda_Contato_Form_activity.kt
+++ Aula_04/app/src/main/java/com/example/aula_04/Agenda_Contato_Form_activity.kt
@@ -1,4 +1,47 @@
 package com.example.aula_04
 
-class agenda_contato_form_layout {
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+data class Contato(
+    var nome: String ="",
+    var email: String ="",
+    var telefone: String =""
+)
+
+class Agenda_Contato_Form_activity: Activity() {
+
+    val lista = ArrayList<Contato>()
+
+    override fun onCreate (bundle: Bundle?){
+        super.onCreate(bundle)
+        setContentView(R.layout.agenda_contato_form_layout)
+
+        val edtNome : EditText = findViewById(R.id.edt_nome)
+        val edtEmail = findViewById<EditText>(R.id.edt_email)
+        val edtTelefone = findViewById<EditText>(R.id.edt_telefone)
+
+        val btnGravar = findViewById<Button>(R.id.btn_gravar)
+        val btnPesquisar = findViewById<Button>(R.id.btn_pesquisar)
+
+        btnGravar.setOnClickListener{
+            val contato = Contato(
+                nome = edtNome.text.toString(),
+                email = edtEmail.text.toString(),
+                telefone = edtTelefone.text.toString()
+            )
+            lista.add(contato)
+
+            Toast.makeText(this,
+                "Contato gravado com sucesso", Toast.LENGTH_LONG).show()
+            edtNome.setText("")
+            edtTelefone.setText("")
+            edtEmail.setText("")
+
+        }
+    }
+
 }
---
+++ Exercicios/CicloVidaActivity/app/src/main/java/com/example/ciclovidaactivity/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.ciclovidaactivity
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.ciclovidaactivity.ui.theme.CicloVidaActivityTheme
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG = "CicloVidaActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+}
+
