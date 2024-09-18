7bb93b94b03434da6f40a1440a00d0c3041d5b58
andrade-thais <thais.andrade11@fatec.sp.gov.br>
2024-09-18 10:59:46 -0300

    Aula 07

--- Aula_07/app/src/main/java/com/example/aula_07/activity/ContatoListaActivity.kt
+++ Aula_07/app/src/main/java/com/example/aula_07/activity/ContatoListaActivity.kt
@@ -2,12 +2,27 @@ package com.example.aula_07.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.aula_07.R
+import com.example.aula_07.model.Contato
+import com.example.aula_07.reciycleView.ContatoAdapter
 
 class ContatoListaActivity : Activity() {
+
+    val lista = ArrayList<Contato>()
 
     override fun onCreate(bundle: Bundle?){
         super.onCreate(bundle)
         setContentView(R.layout.contato_lista_activity)
+
+        lista.add(Contato("Thais Andrade", "(11)1122-3344", "thais@exemple.com"))
+        lista.add(Contato("joao Andrade", "(11)1122-3344", "thais@exemple.com"))
+        lista.add(Contato("maria Andrade", "(11)1122-3344", "thais@exemple.com"))
+
+        val adapter = ContatoAdapter(this, lista)
+        val recycler = findViewById<RecyclerView>(R.id.rcv_contato_lista)
+        recycler.adapter = adapter
+        recycler.layoutManager = LinearLayoutManager(this)
     }
 }
